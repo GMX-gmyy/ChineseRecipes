a859d31a47cdf7d33b61bc15b9a1997486b8fb70
GMX-gmyy <1315974868@qq.com>
2023-08-04 16:54:10 +0800

    first commit

---
+++ ChineseRecipes/CRSupporttingFiles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChineseRecipes
+//
+//  Created by 龚梦洋 on 2023/8/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
